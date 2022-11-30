7d644e43beaf5dc689becffde75bd9d0ffa4443c
- <->
2022-11-30 22:26:04 +0100

    add initial functionality

---
+++ SpeedDejtExp/Person.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeedDejtExp
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public int IDNr { get; set; }
+
+        public string Instagram { get; set; }
+
+        public List<int> Choices { get; set; }
+
+        public List<Person> Matches { get; set; }
+
+        internal void CheckMatches(List<Person> people)
+        {
+            foreach (var person in people)
+            {
+                if (person.Choices.Contains(IDNr) && Choices.Contains(person.IDNr))
+                {
+                    Matches.Add(person);
+                }
+            }
+        
+        }
+
+        public string GetDescription()
+        {
+            if (Matches.Count == 0)
+            {
+                return $"Hej {Name}! Jättekul att du joina oss ikväll!" +
+                    $" Det blev tyvärr ingen matchning för ikväll " +
+                    $"men don't worry vi kommer ha fler event " +
+                    $"med fler roliga människor som dig. " +
+                    $"Håll utkik på vår instagram inför nästa event. " +
+                    $"Vi hoppas verkligen att vi ser dig igen!" +
+                    $" Trevlig kväll!";
+            }
+            else
+            {
+                return $"Hej {Name}! Vad kul att du var med igår kväll " +
+                    $"och vi är inte de enda som tycker det. " +
+                    $"Du matchade med {string.Join(", ", Matches.Select(person => person.Instagram))}. " +
+                    $"Nästa steg är upp till er och vi lämnar över ansvaret på er. " +
+                    $"Hoppas detta blir en perfekt matchning," +
+                    $" annars är du alltid välkommen tillbaka till vårt nästa event!";
+            }
+
+
+        }
+    }
+}
---
+++ SpeedDejtExp/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace SpeedDejtExp
+{
+   
+    internal class Program
+    {
+        public static List<Person> People;
+        static void Main(string[] args)
+        {
+            People= new List<Person>();
+
+            ReadFile();
+            GenerateMatches();
+            ShowMatches();
+            PrintText();
+            WriteToFile();
+        }
+
+        private static void WriteToFile()
+        {
+            File.WriteAllLines(@"C:\Users\MoniraWarsame\Documents\Speed\SpeedDejtExp\Resultat.txt", People.Select(person => person.GetDescription()));
+        }
+
+        private static void PrintText()
+        {
+            foreach (var person in People)
+            {
+                Console.WriteLine(person.GetDescription());
+                Console.WriteLine("------------------");
+            }
+        }
+
+        private static void ShowMatches()
+        {
+            foreach (var person in People)
+            {
+                Console.WriteLine(person.IDNr+ " : " +
+                    person.Name + " Matches with " + string.Join(", ", person.Matches.Select(person=>person.IDNr)));
+                Console.WriteLine();
+            }
+        }
+
+        private static void GenerateMatches()
+        {
+            foreach (var person in People)
+            {
+                person.CheckMatches(People);
+            }
+        }
+
+        static void ReadFile()
+        {
+            var fileLines = File.ReadLines(@"C:\Users\MoniraWarsame\Documents\Speed\SpeedDejtExp\anmalan.tsv").ToList();
+            for (int i = 1; i < fileLines.Count; i++)
+            {
+                var personString = fileLines[i].Split('\t');
+                if (personString[9] == "ja")
+                {
+                    Person person = new Person();
+                    person.Matches = new List<Person>();
+                    person.Name = personString[1];
+                    person.IDNr = Int32.Parse(personString[10]);
+                    person.Instagram = personString[4];
+                    if (!string.IsNullOrEmpty(personString[11]))
+                    {
+
+                     person.Choices = personString[11].Split(',', '.').Select(int.Parse).ToList();
+                    }
+                    else
+                    {
+                        person.Choices = new List<int>();
+                    }
+                    
+                    People.Add(person);
+                }
+            }
+
+        }
+    }
+} 
